0f66d9c0c0af82c6aef9cacc696f4dcadee76d36
gentlewarriormonk <gareth.manning@gmail.com>
2025-03-16 22:00:13 +0100

    Update __init__.py

--- rag_code/__init__.py
+++ rag_code/__init__.py
@@ -3,9 +3,13 @@ RAG Writing Assistant package
 """
 
 # Make sure the modules are importable
-from . import corpus_ingestion
-from . import vector_db
-from . import language_model
-from .rag_assistant import RAGWritingAssistant
+try:
+    from . import corpus_ingestion
+    from . import vector_db
+    from . import language_model
+    from .rag_assistant import RAGWritingAssistant
 
-__all__ = ['RAGWritingAssistant', 'corpus_ingestion', 'vector_db', 'language_model']
+    __all__ = ['RAGWritingAssistant', 'corpus_ingestion', 'vector_db', 'language_model']
+    
+except ImportError as e:
+    print(f"Warning: Not all modules could be imported in rag_code/__init__.py: {e}")
